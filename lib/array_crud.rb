f181b234184b56d9435e642d0c6f50dca8246e23
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-07 16:20:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,9 +8,8 @@ def create_an_array
   
 end
 
-def add_element_to_end_of_array(array, element)[]
-  array.Push("Power")
-  element.Push("Wash")
+def add_element_to_end_of_array(array, element)
+  
 end
 
 def add_element_to_start_of_array(array, element)
