0fca445ef0ffe619fceb9b42ba8498d27ba03231
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-07 16:17:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,7 @@ def create_an_array
   
 end
 
-def add_element_to_end_of_array(array, element)
-  []
+def add_element_to_end_of_array(array, element)[]
   add_element_to_end_of_array.Push  "Power"
 end
 
