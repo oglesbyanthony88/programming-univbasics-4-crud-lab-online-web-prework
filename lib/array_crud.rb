43856d1a1e8876315d03c698c71be7ba1ec9e479
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-07 16:31:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,9 +14,8 @@ element = ["arrays!"]
 end
 
 def add_element_to_start_of_array(array, element)
-  array = ["Wow!"]
-  element = ["I", "am", "really", "learning", "arrays"]
-  
+    array = ["wow", "I", "am", "really", "learning"]
+    element = "arrays!"
 end
 
 def remove_element_from_end_of_array(array)
